876351e62d782e6045681a16ae1817bb05ca03ad
satoshi <bharatrokaya2007@gmail.com>
2026-05-29 09:06:41 +0545

    practice usestate

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Calculate from './App.jsx'
+import NameInput from './Input.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Calculate />
+    <NameInput/>
   </StrictMode>,
 )
